0ee1b97ee94109ca08e7004d1a31bf8818860a78
YIlun Weng <yilunweng@YIluns-iMac-Pro.local>
2019-04-22 14:30:11 -0700

    MVP Completed

--- index.js
+++ index.js
@@ -18,41 +18,79 @@ server.get('/api/users', (req, res) => {
 		})
 })
 server.post('/api/users', (req, res) => {
-	const user = req.body
-	console.log('user',user)
+	const userBody = req.body
+	console.log('user',userBody)
+	if(!userBody.name || !userBody.bio){
+		res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+	}
 	
-	db.insert(user)
+	db.insert(userBody)
 	.then((result) => {
-		res.status(201).json(result)
+		console.log('result',result)
+		if(result){
+			res.status(201).json(result)
+		}
 	})
 	.catch((err) => {
 		console.log('err', err)
-		res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+		res.status(500).json({ error: "There was an error while saving the user to the database" })
 	})
 })
 
 server.get('/api/users/:id', (req, res) => {
-
+	const userId = req.params.id
+	console.log('userId', userId)
+	
+	db.findById(userId)
+	.then((user) => {
+		console.log('resId', user)
+		if(typeof(user) === "undefined"){
+			res.status(404).json({ message: "The user with the specified ID does not exist." })
+		}else{
+			res.status(200).json(user)
+		}
+	})
+	.catch((err) => {
+		console.log('err', err)
+		res.status(500).json({ error: "The user information could not be retrieved." })
+	})
 })
+
 server.delete('/api/users/:id', (req, res) => {
 	const userId = req.params.id
-	console.log('user',userId)
 	db.remove(userId)
-	.then((id) => {
-		console.log('res', id)
-		if(id > 0){
-			res.status(202).json({message: 'Update Successful'})
+	.then((countDeleted) => {
+		console.log('res', countDeleted)
+		if(countDeleted > 0){
+			res.status(202).json({message: 'Delete Successful'})
 		}else{
 			res.status(404).json({message: 'record not found'})
 		}
 	})
 	.catch((err) => {
-		console.log('err', err)
 		res.status(500).json({ message: `there was an error deleting ${err}`})
 	})
 })
 server.put('/api/users/:id', (req, res) => {
-
+	const userId = req.params.id
+	const userBody = req.body
+	
+	if(!userBody.name || !userBody.bio){
+		res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+	}
+	
+	db.update(userId, userBody)
+	.then((user) => {
+		console.log('user', user)
+		if(user > 0){
+			res.status(200).json( user)
+		}else{
+			res.status(404).json( { message: "The user with the specified ID does not exist." })
+		}
+	})
+	.catch((err) => {
+		res.status(500).json({ error: "The user information could not be modified." })
+	})
 })
 
 server.listen(5000, ()=>{
