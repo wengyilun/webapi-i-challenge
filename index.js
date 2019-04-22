f18585b8ed54cf19768d762d4ec2b1a9238360d5
YIlun Weng <yilunweng@YIluns-iMac-Pro.local>
2019-04-22 13:03:18 -0700

    Add GET users end point

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+
+const express = require('express')
+const db = require('./data/db')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+	db.find()
+		.then((result) => {
+			res.status(200).json(result)
+		})
+		.catch((err) => {
+			console.log('err', err)
+			res.status(500).json({ error: "The users information could not be retrieved." })
+		})
+})
+server.post('/api/users', (req, res) => {
+
+})
+
+server.get('/api/users/:id', (req, res) => {
+
+})
+server.delete('/api/users/:id', (req, res) => {
+
+})
+server.put('/api/users/:id', (req, res) => {
+
+})
+
+server.listen(5000, ()=>{
+	console.log('\n***Server is listening on port 5000\n')
+})
